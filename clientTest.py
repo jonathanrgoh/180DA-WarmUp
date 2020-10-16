48722dab122e73a6a1a89d9287f0b1e1fc1c86da
Jonathan Goh <42307585+jonathanrgoh@users.noreply.github.com>
2020-10-15 23:13:57 -0700

    server/client pt.2

--- clientTest.py
+++ clientTest.py
@@ -1,6 +1,6 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('0.0.0.0', 8080))
+client.connect(('DESKTOP-C5T6AL1', 1234))
 client.send("I am CLIENT\n")
 from_server = client.recv(4096)
 client.close()
--- serverTest.py
+++ serverTest.py
@@ -1,12 +1,18 @@
 # Reminder: This is a comment. The first line imports a default library "socket" into Python.
 # You don’t install this. The second line is initialization to add TCP/IP protocol to the endpoint.
 import socket
+import cv2
+
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Assigns a port for the server that listens to clients connecting to this port.
-serv.bind(('0.0.0.0', 8080))
+host = socket.gethostname()
+print(host)
+serv.bind((host, 1234))
 serv.listen(5)
 
 while True:
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
     conn, addr = serv.accept()
     from_client = ''
     while True:
@@ -15,5 +21,7 @@ while True:
         from_client += data
         print(from_client)
         conn.send("I am SERVER\n")
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
     conn.close()
     print('client disconnected')
